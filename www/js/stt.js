1c19e3a8c6a499536c3d3e0ac058f09a615e8012
leocantam13-ctrl <leocantam13@gmail.com>
2026-01-19 19:19:27 -0300

    Delete www/js directory

--- www/js/stt.js
+++
@@ -1,9 +0,0 @@
-// Reconhecimento de voz
-const LionelSTT = (() => {
-  function init(callback) {
-    if(!('webkitSpeechRecognition' in window)) return;
-    const recognition = new webkitSpeechRecognition();
-    recognition.lang = 'pt-BR';
-    recognition.onresult = e => callback(e.results[0][0].transcript);
-  }
-})();
